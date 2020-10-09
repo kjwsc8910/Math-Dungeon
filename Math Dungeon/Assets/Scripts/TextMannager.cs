11fad94930d42c2fc2b7a4ae2de99d81b55f31b2
kjwsc8910 Laptop <05kj@sky.com>
2020-10-09 13:29:24 +0100

    Lesson

--- Math Dungeon/Assets/Scripts/TextMannager.cs
+++ Math Dungeon/Assets/Scripts/TextMannager.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class TextMannager : MonoBehaviour
 {
+    private GameObject textBox;
+    private GameObject combatBox;
+
     void Start()
     {
-        
+        textBox = GameObject.FindGameObjectWithTag("TextBox");
+        combatBox = GameObject.FindGameObjectWithTag("CombatBox");
     }
+
+    public void ShowText(string _text)
+	{
+
+	}
 }
